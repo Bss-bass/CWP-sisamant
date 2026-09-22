152f4222fe8b90fe8e7d496553613f72a4c82eb4
Bss-bass <sivapat13@gmail.com>
2026-09-22 21:46:51 +0700

    feat(cell06): add ex03

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,12 @@
+def greetings(name=None):
+    if type(name) == str:
+        print(f"Hello {name}")
+    elif type(name) == int:
+        print("Error! It was not a name.")
+    else:
+        print("Hello, noble stranger.")
+
+greetings('Alexandra')
+greetings('Wil')
+greetings()
+greetings(42)
